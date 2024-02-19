8730f514fb720ae05b6958b35d567aa696518e3f
Nico Rithner <nicorithner@gmail.com>
2024-02-19 07:47:04 -0700

    Organize card jsx sections

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,15 +8,21 @@ export default function Card() {
         src="../../../public/assets/images/avatar-jessica.jpeg"
         alt="user picture"
       />
-      <h1>Jessica Randall</h1>
-      <h2>London, United Kingdom</h2>
-      <h3>Front-end developer and avid reader.</h3>
+      <div className="card-title">
+        <h1>Jessica Randall</h1>
+        <h2>London, United Kingdom</h2>
+      </div>
+      <h3 className="card-user-summary">
+        Front-end developer and avid reader.
+      </h3>
 
-      <p>GitHub</p>
-      <p>Frontend Mentor</p>
-      <p>LinkedIn</p>
-      <p>Twitter</p>
-      <p>Instagram</p>
+      <div className="links-section">
+        <a className="link-bar">GitHub</a>
+        <a className="link-bar">Frontend Mentor</a>
+        <a className="link-bar">LinkedIn</a>
+        <a className="link-bar">Twitter</a>
+        <a className="link-bar">Instagram</a>
+      </div>
     </div>
   );
 }
